622fb4df6cc57447ff0729920b413f55c80badc2
hermeslmj <hermeslmj@gmail.com>
2012-10-11 19:30:05 -0300

    Criação de um modulo para representar o controle e outro para representar o modelo do gerenciador de configurações.

---
+++ configuracao/controle/index.js
@@ -0,0 +1,16 @@
+
+function ControleConfiguracao(){
+	var modeloConfiguracao;
+	
+	
+}
+
+ControleConfiguracao.prototype.novaConfiguracao =  function(){
+	
+}
+ControleConfiguracao.prototype.editarConfiguracao =  function(){
+	
+}
+ControleConfiguracao.prototype.apagarConfiguracao =  function(){
+	
+}
---
+++ configuracao/modelo/index.js
@@ -0,0 +1,43 @@
+
+function ModeloConfiguracao(){
+	var usuario;
+	var senha;
+	var banco;
+	var host;
+	var GerenciadorDeArquivos = require('../arquivos');
+}
+
+ModeloConfiguracao.prototype.setNome = function(nome){
+	
+}
+ModeloConfiguracao.prototype.setSenha = function(senha){
+	
+}
+ModeloConfiguracao.prototype.setBanco = function(banco){
+	
+}
+ModeloConfiguracao.prototype.setHost = function(host){
+	
+}
+ModeloConfiguracao.prototype.getNome = function(){
+	
+}
+ModeloConfiguracao.prototype.getSenha = function(){
+	
+}
+ModeloConfiguracao.prototype.getBanco = function(){
+	
+}
+ModeloConfiguracao.prototype.getHost = function(){
+	
+}
+ModeloConfiguracao.prototype.gravarConfiguracao = function(){
+	
+}
+ModeloConfiguracao.prototype.editarConfiguracao = function(){
+	
+}
+ModeloConfiguracao.prototype.apagarConfiguracao = function(){
+	
+}
+
